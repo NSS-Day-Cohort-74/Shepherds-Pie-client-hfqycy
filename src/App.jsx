c55b0a2a49c35c023390b689d698be8d37d51816
Brian Henry <brianhenrytech@gmail.com>
2025-01-22 15:32:58 -0600

    Fixed file names

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import "./App.css";
-import { Routes, Route} from "react-router-dom";
-import { Login } from "./components/auth/login";
+import { Routes, Route } from "react-router-dom";
+import { Login } from "./components/auth/Login";
 
 
 //useState()(REACT) = const [stateVariable, setterFunction], must pass in an initial value for useState("[], 0, '', boolean"), must be imported from REACT
 export const App = () => {
   return (
     <Routes>
-      <Route path="/login" element={<Login />}/>
-     </Routes>
+      <Route path="/login" element={<Login />} />
+    </Routes>
   );
 };
