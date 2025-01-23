9fdd6702bfe5d4a3ba92e1752696e7eb489e7a9d
Brian Henry <brianhenrytech@gmail.com>
2025-01-23 14:32:47 -0600

    updated styling for Employee Details Component

--- src/pages/EmployeeDetails.jsx
+++ src/pages/EmployeeDetails.jsx
@@ -1,8 +1,8 @@
 import { useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
 import { getEmployeeById } from "../services/getEmployeeById"
+import { updateEmployee } from "../services/updateEmployeeDetails"
 import "./EmployeeDetails.css"
-import { updateEmployee } from "../services/updateEmployeeDetails";
 
 export const EmployeeDetails = () => {
   const [employee, setEmployee] = useState({})
@@ -10,45 +10,43 @@ export const EmployeeDetails = () => {
 
   useEffect(() => {
     const fetchEmployee = async () => {
-      const employeeData = await getEmployeeById(employeeId);
-      setEmployee(employeeData);
-    };
-    fetchEmployee();
-  }, [employeeId]);
+      const employeeData = await getEmployeeById(employeeId)
+      setEmployee(employeeData)
+    }
+    fetchEmployee()
+  }, [employeeId])
 
   const handleInputChange = (e) => {
     setEmployee({
       ...employee,
       [e.target.name]: e.target.value
-    });
-  };
+    })
+  }
 
   const handleSaveChanges = () => {
     updateEmployee({ ...employee }).then(() => {
       window.alert("Employee updated")
     })
-
-  };
+  }
 
   return (
-    <div className="employee-form-container">
-      <div className="employee-form">
+    <div className="employee-details-container">
+      <div className="employee-details-form">
         <h1>{employee.name}</h1>
-        <div>
+        <div className="form-group">
           <h4>Address</h4>
-          <input type="text" defaultValue={employee.address} onChange={handleInputChange} name="address"></input>
+          <input type="text" defaultValue={employee.address} onChange={handleInputChange} name="address" />
         </div>
-        <div>
+        <div className="form-group">
           <h4>Email</h4>
-          <input type="text" defaultValue={employee.email} onChange={handleInputChange} name="email"></input>
+          <input type="text" defaultValue={employee.email} onChange={handleInputChange} name="email" />
         </div>
-        <div>
+        <div className="form-group">
           <h4>Phone Number</h4>
-          <input type="text" defaultValue={employee.phoneNumber} onChange={handleInputChange} name="phoneNumber"></input>
+          <input type="text" defaultValue={employee.phoneNumber} onChange={handleInputChange} name="phoneNumber" />
         </div>
         <button onClick={handleSaveChanges}>Save Changes</button>
       </div>
     </div>
-  );
-};
-
+  )
+}
